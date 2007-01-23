3ff175ef63da32083b72aad29ce2447b65b36983
reddaly <reddaly@a2798843-a326-0410-8c20-e73a9a70bbad>
2007-01-23 23:26:43 +0000

    no changes to given code

---
+++ homework/cs108/hw2-tetris/JBrainTetris.java
@@ -0,0 +1,19 @@
+// JBrainTetris
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ Subclass of JTetris that adds in brain features.
+ Works with a few key overrides.
+ JTetris.main() can create either a JTetris or a JBrainTetris.
+*/
+public class JBrainTetris extends JTetris {
+	
+    public JBrainTetris(int width, int height) {
+        super(width, height);
+    }
+    
+    // YOUR CODE HERE
+}
