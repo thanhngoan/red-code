344794ece87fb7a9982e170345ae3ce59f81a0b0
reddaly <reddaly@a2798843-a326-0410-8c20-e73a9a70bbad>
2007-01-23 23:26:43 +0000

    no changes to given code

---
+++ trunk/homework/cs108/hw2-tetris/BadBrain.java
@@ -0,0 +1,12 @@
+// BadBrain.java
+/**
+ A joke implementation based on LameBrain --
+ plays very, very badly.
+*/
+public class BadBrain extends LameBrain {
+
+	public double rateBoard(Board board) {
+		double score = super.rateBoard(board);
+		return(10000 - score);
+	}
+}
