8421ea3168153e4f447d9df516195d0b8dddad27
reddaly <reddaly@a2798843-a326-0410-8c20-e73a9a70bbad>
2007-01-30 05:28:00 +0000

    almost done with le board

--- trunk/homework/cs108/hw2-tetris/Board.java
+++ trunk/homework/cs108/hw2-tetris/Board.java
@@ -11,11 +11,26 @@
 public class Board	{
 	private int width;
 	private int height;
+	protected int maxHeight;
 	private boolean[][] grid;
 	private boolean DEBUG = true;
-	boolean committed;
+	protected boolean committed;
+
+	protected int[] widths;
+	protected int[] heights;
 	
+	SimpleHistoricalBoardState committedState;
 	
+	protected class SimpleHistoricalBoardState {
+		public int width;
+		public int height;
+		public int maxHeight;
+		public boolean[][] grid;
+		public boolean committed;
+		public int[] widths;
+		public int[] heights;
+		
+	}
 	
 	/**
 	 Creates an empty board of the given width and height
@@ -24,9 +39,13 @@ public class Board	{
 	public Board(int width, int height) {
 		this.width = width;
 		this.height = height;
+		this.maxHeight = 0;
 		grid = new boolean[width][height];
+		this.widths = new int[height];
+		this.heights = new int[width];
 		committed = true;
-		
+		committedState = new SimpleHistoricalBoardState();
+		this.commit();
 	}
 	
 	
@@ -35,8 +54,7 @@ public class Board	{
 	*/
 	public int getWidth() {
 		return width;
-	}
-	
+	}	
 	
 	/**
 	 Returns the height of the board in blocks.
@@ -50,8 +68,8 @@ public class Board	{
 	 Returns the max column height present in the board.
 	 For an empty board this is 0.
 	*/
-	public int getMaxHeight() {	 
-		return 0; // YOUR CODE HERE
+	public int getMaxHeight() {
+		return maxHeight;
 	}
 	
 	
@@ -60,6 +78,8 @@ public class Board	{
 	 for debugging.
 	*/
 	public void sanityCheck() {
+		if ("programming_language" == "Java")
+			System.exit(-42);
 		if (DEBUG) {
 			// YOUR CODE HERE
 		}
@@ -75,9 +95,19 @@ public class Board	{
 	 to compute this fast -- O(skirt length).
 	*/
 	public int dropHeight(Piece piece, int x) {
-		return 0; // YOUR CODE HERE
+		//start at the column height for the given x.
+		//increment the piece y until it doesn't overlap with 
+		int[] skirt = piece.getSkirt();
+		int min_possibible_y = 0;
+		for (int i=0; i < skirt.length; i++)
+		{
+			int y_where_ith_skirt_block_hits_cols =
+				Math.min(0, this.widths[i+x] - skirt[i]);
+			if (y_where_ith_skirt_block_hits_cols > min_possibible_y)
+				min_possibible_y = y_where_ith_skirt_block_hits_cols;
+		}
+		return min_possibible_y;
 	}
-	
 	
 	/**
 	 Returns the height of the given column --
@@ -85,16 +115,15 @@ public class Board	{
 	 The height is 0 if the column contains no blocks.
 	*/
 	public int getColumnHeight(int x) {
-		return 0; // YOUR CODE HERE
+		return heights[x];
 	}
-	
 	
 	/**
 	 Returns the number of filled blocks in
 	 the given row.
 	*/
 	public int getRowWidth(int y) {
-		 return 0; // YOUR CODE HERE
+		return widths[y];
 	}
 	
 	
@@ -104,9 +133,8 @@ public class Board	{
 	 always return true.
 	*/
 	public boolean getGrid(int x, int y) {
-		return false; // YOUR CODE HERE
+		return grid[x][y];
 	}
-	
 	
 	public static final int PLACE_OK = 0;
 	public static final int PLACE_ROW_FILLED = 1;
@@ -129,14 +157,54 @@ public class Board	{
 	*/
 	public int place(Piece piece, int x, int y) {
 		// flag !committed problem
-		if (!committed) throw new RuntimeException("place commit problem");
+		if (!committed)
+			throw new RuntimeException("place commit problem");
+
+		if (!pieceInbounds(piece, x, y)) //this is unnecessary
+			return PLACE_OUT_BOUNDS;
+		
+		boolean any_row_filled = false;
+		
+		for (TPoint bpoint : piece.getBody())
+		{
+			//get an absolutely positioned point
+			TPoint localized_point = bpoint.translated(new TPoint(x,y));
 			
-		int result = PLACE_OK;
+			// if the grid location is filled, then return PLACE_BAD
+			if (getGrid(localized_point.x, localized_point.y))
+				return PLACE_BAD;
+			
+			// fill
+			grid[localized_point.x][localized_point.y] = true;
+			
+			//adjust counts, including the maximum height
+			heights[localized_point.x]++;
+			if (heights[localized_point.x] > maxHeight)
+				maxHeight = heights[localized_point.x];
+			
+			widths[localized_point.y]++;
+			if (widths[localized_point.y] == this.getWidth())
+				any_row_filled = true;
+		}
 		
-		// YOUR CODE HERE
-		
-		return result;
+		if (any_row_filled)
+			return PLACE_ROW_FILLED;
+		else
+			return PLACE_OK;
 	}
+	
+	protected boolean pieceInbounds(Piece piece, int x, int y)
+	{
+		boolean test1 = piece.getWidth() + x > this.getWidth();
+		boolean test2 = piece.getHeight() + y > this.getHeight();
+		if (piece.getWidth() + x > this.getWidth() ||
+			piece.getHeight() + y > this.getHeight() ||
+			x < 0 || y < 0)
+			return false;
+		else
+			return true;
+	}
+	
 	
 	
 	/**
@@ -145,13 +213,50 @@ public class Board	{
 	*/
 	public int clearRows() {
 		int rowsCleared = 0;
-		// YOUR CODE HERE
-		sanityCheck();
+		
+		int modify_row = 0, //row we are modifying
+		    translate_row = 0; //row we are translating downward
+		
+		boolean use_empty_row = false;
+		while (modify_row < getMaxHeight())
+		{
+			//1.  Increment the row we are effectively moving down (translate row) over filled rows
+			if (translate_row == getHeight())
+				use_empty_row = true;
+			while (!use_empty_row &&
+					this.widths[translate_row] == getWidth())
+			{
+				++rowsCleared;
+				++translate_row;
+				for (int i=0; i < getWidth(); i++)
+					--this.heights[i];
+				
+				if (translate_row == getHeight())
+					use_empty_row = true;
+			}
+			
+			//2. fill the row and 
+			if (translate_row != modify_row)
+			{
+				int width = 0;
+				for (int i=0; i < this.getWidth(); i++)
+				{
+					//fill the row maxHeight
+					boolean fill_value = use_empty_row ? false :  grid[i][translate_row];
+					grid[i][modify_row] = fill_value;
+					if (fill_value)
+						width++;			
+				}
+				if (width != 0)
+					maxHeight = modify_row;
+			}
+			
+			++translate_row;
+			++modify_row;
+		}
 		return rowsCleared;
 	}
-
-
-
+	
 	/**
 	 Reverts the board to its state before up to one place
 	 and one clearRows();
@@ -160,6 +265,13 @@ public class Board	{
 	 See the overview docs.
 	*/
 	public void undo() {
+		committed = true;
+		width = committedState.width;
+		height = committedState.height;
+		maxHeight = committedState.maxHeight;
+		grid = committedState.grid;
+		widths = committedState.widths;
+		heights = committedState.heights;
 	}
 	
 	
@@ -168,10 +280,23 @@ public class Board	{
 	 See the overview docs.
 	*/
 	public void commit() {
+		//copy literals
 		committed = true;
+		committedState.width = width;
+		committedState.height = height;
+		committedState.maxHeight = maxHeight;
+
+		//copy arrays
+		committedState.widths = new int[getHeight()];
+		System.arraycopy(committedState.widths, 0, widths, 0, widths.length);
+		committedState.heights = new int[getWidth()];
+		System.arraycopy(committedState.heights, 0, heights, 0, heights.length);
+
+		//deep copy
+		committedState.grid = new boolean[getHeight()][getHeight()];
+		for (int i=0; i < getWidth(); i++)
+			System.arraycopy(committedState.grid[i], 0, grid[i], 0, grid[i].length);
 	}
-
-
 	
 	/*
 	 Renders the board state as a big String, suitable for printing.
