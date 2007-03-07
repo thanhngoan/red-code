6c1c77054171e83a80e4b9b42ca51ec337ce97c8
reddaly <reddaly@a2798843-a326-0410-8c20-e73a9a70bbad>
2007-03-07 05:26:44 +0000

    debugging and writing unit tests

--- homework/ee108b/pa2/scrap.s
+++ homework/ee108b/pa2/scrap.s
@@ -8,6 +8,16 @@ li $v0, 4
 syscall
 move $a0, $t1           # number
 
+#print a number (in $t2)
+move $t2, $a0           # number
+move $a0, $s0           # number
+li $v0, 1
+syscall
+la $a0, retn            # end of line
+li $v0, 4
+syscall
+move $a0, $t2           # number
+
 ##DEBUG
 move $t2, $a0           # number
 move $a0, $s1           # number
